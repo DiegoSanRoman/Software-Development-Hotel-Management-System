8ddcc3383d99969d89ab8f0c929423cdc6a97b01
Diegsrp4 <113380669+Diegsrp4@users.noreply.github.com>
2024-03-11 11:52:26 +0100

    I changed the validatecreditcardmethod

--- UC3MTravel/HotelManager.py
+++ UC3MTravel/HotelManager.py
@@ -9,7 +9,27 @@ class HotelManager:
     def validatecreditcard( self, x ):
         # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
         # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-        return True
+
+        # Step 1: Transform the 16-digit number to a list containing the digits
+        # of the number
+        numbers = [int(digit) for digit in str(x)]
+
+        # Step 2: Double every second digit starting from the right according to
+        # the Luhn algorithm
+        for i in range(len(numbers) - 2, -1, -2):
+            numbers[i] *= 2
+            if numbers[i] > 9:
+                numbers[i] -= 9
+
+        # Step 3: Sum all the digits
+        total = 0
+        for i in range(len(numbers)):
+            total += numbers[i]
+
+        # Step 4: Check if the total module 10 is equal to 0. If it is, then the
+        # credit card number is correct, and we return True. If it is not,
+        # then the credit card number is incorrect, and we return False.
+        return total % 10 == 0
 
     def ReaddatafromJSOn( self, fi):
 
