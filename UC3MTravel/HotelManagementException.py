7241a5c5ce0cd23dbf9934071a69298f0d0c74c2
izanUC3M <100495774@alumnos.uc3m.es>
2024-03-18 15:20:31 +0100

    son cambios para que pylint no de errores

--- UC3MTravel/HotelManagementException.py
+++ UC3MTravel/HotelManagementException.py
@@ -1,13 +1,17 @@
-class HotelManagementException(Exception):
+"""gdfhgjhkjkhjghfgdf"""
+class hotelManagementException(Exception):
+    """HlwmkcnoaibcLIVOÑLEAHSEBLCSDLINHWKASKXFKHLILAJ"""
     def __init__(self, message):
         self.__message = message
         super().__init__(self.message)
 
     @property
     def message(self):
+        """ASDUGVNACIXMUHJSEYGDNFSILUMSKSIUD"""
         return self.__message
 
     @message.setter
     def message(self,value):
+        """awivedcbnqxwihzgnvxoedrsjvrfbdsnwiaysdulirufbvnc4jgredwuiu"""
         self.__message = value
 
--- UC3MTravel/__init__.py
+++ UC3MTravel/__init__.py
@@ -1,3 +1,4 @@
+
 from UC3MTravel.HotelReservation import HotelReservation
 from UC3MTravel.HotelManager import HotelManager
 from UC3MTravel.HotelManagementException import HotelManagementException
