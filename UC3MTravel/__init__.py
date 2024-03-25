f6164b14c840a88bf17cbe725107f74c13d5b505
izanUC3M <100495774@alumnos.uc3m.es>
2024-03-25 11:58:59 +0100

    he arreglado varios errores de pylint

--- UC3MTravel/__init__.py
+++ UC3MTravel/__init__.py
@@ -85,7 +85,7 @@ def guest_arrival(file_path):
     myStay = hotelStay(id, localizer, numdays, roomtype)
     if arrival == myStay.arrival:
         print("All the information is correct, processing the room key")
-        print("Room key: %s" % myStay.roomKey)
+        print(f"Room key: {myStay.roomKey}")
         return myStay.roomKey
 
 
