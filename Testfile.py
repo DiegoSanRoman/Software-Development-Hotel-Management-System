36c4158599777cd64ca3c6bd9f35ce7399ad28c0
izanUC3M <100495774@alumnos.uc3m.es>
2024-03-27 13:27:55 +0100

    I am creating one json file for each test

--- Testfile.py
+++ Testfile.py
@@ -8,21 +8,10 @@ class testGuestArrival(unittest.TestCase):
 
     def testTc1Valid(self):
         """Valid case in which all the information is correct"""
-        # Data for the test
-        testData = {
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "IdCard": 1236
-        }
-        testList = [testData]
-        # JSON name
-        fileName = "test.json"
-
-        # Write data in json file
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile)
 
         myManager = HotelManager()
-        filePath = "test.json"  # Use the file test.json
+        filePath = "JSONtestsFunction2/TC1Valid.json"  # Use the file
+        # TC1Valid.json
         value = myManager.guest_arrival(filePath)
         self.assertEqual(value, '256a59af85d250a299da5d62bb797f7a1a1ef1007737f6b1dcc2d48114c21ac6')
 
@@ -30,7 +19,39 @@ class testGuestArrival(unittest.TestCase):
         """Test for the second case, empty file"""
         # JSON empty file name
         myManager = HotelManager()
-        filePath = "JSONtestsFunction2/emptyforTC2.json"
+        filePath = "JSONtestsFunction2/TC2.json"
+        exception = None
+        try:
+            myManager.guest_arrival(filePath)
+        except hotelManagementException as e:
+            exception = e
+
+        # Verify if there was an exception and if the correct message was
+        # displayed
+        self.assertIsNotNone(exception)
+        self.assertEqual(str(exception), "File not found or empty file")
+    def testTc3(self):
+        """Test for the second case, duplicated files FileFile (without
+        separator)"""
+        # JSON empty file name
+        myManager = HotelManager()
+        filePath = "JSONtestsFunction2/TC3.json"
+        exception = None
+        try:
+            myManager.guest_arrival(filePath)
+        except hotelManagementException as e:
+            exception = e
+
+        # Verify if there was an exception and if the correct message was
+        # displayed
+        self.assertIsNotNone(exception)
+        self.assertEqual(str(exception), "File not found or empty file")
+    def testTc4(self):
+        """Test for the 4th case, DEL/MOD/DUP. In this case we just check
+        MOD, since with the other two the result will be the same.)"""
+        # JSON empty file name
+        myManager = HotelManager()
+        filePath = "JSONtestsFunction2/TC4MOD.json"
         exception = None
         try:
             myManager.guest_arrival(filePath)
@@ -43,21 +64,12 @@ class testGuestArrival(unittest.TestCase):
         self.assertEqual(str(exception), "File not found or empty file")
     def testTc5(self):
         """Test for the fifth case, empty data"""
-        # Data for the test
-        testData = {
-        }
-        testList = [testData]
-        # JSON name
-        fileName = "test.json"
-
-        # Write data in json file
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile)
         # JSON empty file name
         myManager = HotelManager()
+        filePath = "JSONtestsFunction2/TC5.json"
         exception = None
         try:
-            myManager.guest_arrival(fileName)
+            myManager.guest_arrival(filePath)
         except hotelManagementException as e:
             exception = e
 
@@ -67,38 +79,16 @@ class testGuestArrival(unittest.TestCase):
         self.assertEqual(str(exception), "'Localizer' key missing in JSON")
     def testTc6(self):
         """Test for the sixth case, duplicated data"""
-        # Data for the test
-        testData = {
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "IdCard": 1236,
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "IdCard": 1236
-        }
-        testList = [testData]
-        # JSON name
-        fileName = "test.json"
-
-        # Write data in json file
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile)
-        # JSON empty file name
         myManager = HotelManager()
-        value = myManager.guest_arrival(fileName)
-        self.assertEqual(value, '256a59af85d250a299da5d62bb797f7a1a1ef1007737f6b1dcc2d48114c21ac6')
+        filePath = "JSONtestsFunction2/TC6.json"  # Use the file test.json
+        value = myManager.guest_arrival(filePath)
+        self.assertEqual(value,
+                         '256a59af85d250a299da5d62bb797f7a1a1ef1007737f6b1dcc2d48114c21ac6')
     def testTc7F1DEL(self):
-        """Test for the seventh case, one of the fields del or dup"""
-        # Data for the test
-        testData = {
-            "IdCard": 1236
-        }
-        testList = [testData]
-        # JSON name
-        fileName = "test.json"
+        """Test for the seventh case, one of the fields is deleted. In this
+        case only F1."""
 
-        # Write data in json file
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile)
-        # JSON empty file name
+        fileName = "JSONtestsFunction2/TC7F1.json" # JSON empty file name
         myManager = HotelManager()
         exception = None
         try:
@@ -111,19 +101,11 @@ class testGuestArrival(unittest.TestCase):
         self.assertIsNotNone(exception)
         self.assertEqual(str(exception), "'Localizer' key missing in JSON")
     def testTc7F2DEL(self):
-        """Test for the seventh case, one of the fields del or dup"""
-        # Data for the test
-        testData = {
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762"
-        }
-        testList = [testData]
-        # JSON name
-        fileName = "test.json"
+        """Test for the seventh case, one of the fields is deleted, in this
+        case F2."""
 
-        # Write data in json file
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile)
-        # JSON empty file name
+        # JSON name
+        fileName = "JSONtestsFunction2/TC7F2.json"
         myManager = HotelManager()
         exception = None
         try:
@@ -136,60 +118,18 @@ class testGuestArrival(unittest.TestCase):
         self.assertIsNotNone(exception)
         self.assertEqual(str(exception), "'Id' key missing in JSON")
     def testTc8F1DUP(self):
-        """Test for the seventh case, one of the fields del or dup"""
-        # Data for the test
-        testData = {
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "IdCard": 1236
-        }
-        testList = [testData]
+        """Test for the seventh case, one of the fields is duplicated"""
         # JSON name
-        fileName = "test.json"
-
-        # Write data in json file
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile)
-        # JSON empty file name
-        myManager = HotelManager()
-        value = myManager.guest_arrival(fileName)
-        self.assertEqual(value, '256a59af85d250a299da5d62bb797f7a1a1ef1007737f6b1dcc2d48114c21ac6')
-    def testTc8F2DEL(self):
-        """Test for the seventh case, one of the fields del or dup"""
-        # Data for the test
-        testData = {
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "IdCard": 1236,
-            "IdCard": 1236
-        }
-        testList = [testData]
-        # JSON name
-        fileName = "test.json"
-
-        # Write data in json file
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile)
-        # JSON empty file name
+        fileName = "JSONtestsFunction2/TC8.json"
         myManager = HotelManager()
         value = myManager.guest_arrival(fileName)
         self.assertEqual(value, '256a59af85d250a299da5d62bb797f7a1a1ef1007737f6b1dcc2d48114c21ac6')
 
     def testTc9MOD(self):
         """Test for the ninth case, separator is modified"""
-        # Datos para el test con separador modificado
-        testData = {
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "IdCard": 1236
-        }
-        testList = [testData]
 
         # JSON file name
-        fileName = "test.json"
-
-        # write data in JSON with modified separator
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile, separators=(';', ':'))
-
+        fileName = "JSONtestsFunction2/TC9MOD.json"
         myManager = HotelManager()
         exception = None
         try:
@@ -202,19 +142,9 @@ class testGuestArrival(unittest.TestCase):
         self.assertEqual(str(exception), "File not found or empty file")
     def testTc9DUP(self):
         """Test for the ninth case, separator is duplicated"""
-        # Datos para el test con separador modificado
-        testData = {
-            "Localizer": "d32ff67ec38aab9dda77c20b4aef2762",
-            "IdCard": 1236
-        }
-        testList = [testData]
 
         # JSON file name
-        fileName = "test.json"
-
-        # write data in JSON with modified separator
-        with open(fileName, "w", encoding='utf-8') as jsonFile:
-            json.dump(testList, jsonFile, separators=(',,', ';;'))
+        fileName = "JSONtestsFunction2/TC9DUP.json"
 
         myManager = HotelManager()
         exception = None
