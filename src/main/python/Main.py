f4a4b1c6450c77c1adde4a81e5d46ac693ce2a3b
Diegsrp4 <113380669+Diegsrp4@users.noreply.github.com>
2024-04-02 18:16:34 +0200

    de nada equipo, aquí padreando

--- src/main/python/Main.py
+++ src/main/python/Main.py
@@ -8,16 +8,13 @@ def main():
     """Function to try and check methods and functions"""
     # MAIN PROGRAM
     hotel = hotelManager()
-    """info = (
-        5555555555554444, "Barbara Sanchez", 456, 123456788, "single",
-        "2024-01-02", 1)
-    print(hotel.roomReservation(*info))"""
     jsonPath = (str(Path.home()) +
                 "/G88.2024.T05.GE2/src/JSONfiles/JsonForFunctions/Arrival.json")
+    info = (
+        5555555555554444, "Santiago P", 100, 100000000, "single",
+        "2024-01-01", 1)
+    print(hotel.roomReservation(*info))
     hotel.guestArrival(jsonPath)
-    """result3 = hotel.guestCheckout('e21263a92c0651e895a03558af54cf71399af99094e0f041063ed8c676241355')
-    print(result3)"""
-
 
 
 
