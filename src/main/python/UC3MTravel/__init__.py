0598fe50a4e5fe1671f4c7977686624a8bd9c69b
izanUC3M <100495774@alumnos.uc3m.es>
2024-04-02 19:03:05 +0200

    ALMOST FINAL COMMIT

--- src/main/python/UC3MTravel/__init__.py
+++ src/main/python/UC3MTravel/__init__.py
@@ -1,34 +1,3 @@
-"""For now, we are using this file to record information about the functions
-of GE2. Then we will store those functions in HotelManager"""
-from pathlib import Path
-from freezegun import freeze_time
-from UC3MTravel.HotelManager import hotelManager
-@freeze_time('2024-01-01')
+"""We have used this file for checking code and functions, now that we don't
+need it, we just leave it blank not to create any errors in pybuilder"""
 
-
-# Main
-def main():
-    """A main created to try and see if the function works"""
-    # Define the relative path to the file
-    myManager = hotelManager()
-    '''
-    filePath = (str(Path.home()) +
-                "\G88.2024.T05"
-                ".GE2\Arrival.json")
-    myManager.guestArrival(filePath)
-    # ROOM KEY = 256a59af85d250a299da5d62bb797f7a1a1ef1007737f6b1dcc2d48114c21ac6
-    '''
-
-
-    # Tests for the development of function 3
-    filePath = (str(Path.home()) + "\G88.2024.T05.GE2\src\JSONfiles\JsonForFunctions\Arrival.json")
-    info = (5555555555554444, "Barbara Sanchez", 456, 123456788, "single", "2024-01-01", 1)
-    # localizer = 96350e37cb66e2d6c5143ad15fa62060
-    # id = 456
-    #print(myManager.roomReservation(*info))
-    #print(myManager.guestArrival(filePath))
-    myManager.guestCheckout("")
-
-
-if __name__ == "__main__":
-    main()
